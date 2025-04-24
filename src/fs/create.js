8946c097f4df97626d4af615971e99d46b18b1d4
dsalangin <dsalangin74@gmail.com>
2025-04-25 00:14:08 +0300

    feat: implement file creation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import {writeFile} from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fresh.txt');
+    await writeFile(pathToFile, 'I am fresh and young', {flag: 'wx'}).catch((err) => {
+        if(err.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err; 
+        }
+    }) ;
 };
 
 await create();

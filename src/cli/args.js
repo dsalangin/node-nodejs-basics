5a6d67e5dcc27e6b55ec2fde3d792f1273d00b04
dsalangin <dsalangin74@gmail.com>
2025-04-26 18:33:22 +0300

    feat: implement display args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const result = [];
+
+    for (let i = 2; i < process.argv.length; i++) {
+        if(!process.argv[i].startsWith('--')) {
+            continue;
+        }
+
+        result.push(`${process.argv[i].slice(2)} is ${process.argv[i + 1]}`);
+    }
+
+    console.log(result.join(', '));
 };
 
 parseArgs();

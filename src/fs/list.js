b4cc057c3d8cc5641f996f83f9d73f91db0c3f01
dsalangin <dsalangin74@gmail.com>
2025-04-26 15:55:00 +0300

    feat: implement file list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
-};
+    const pathToFolder = join(__dirname, 'files');
+
+    const filenames = await readdir(pathToFolder)
+        .catch((err) => {
+            if (err.code !== 'ENOENT') {
+                throw err
+            }
+
+            throw new Error('FS operation failed');
+        });
+
+        console.log(...filenames);
+    };
 
 await list();

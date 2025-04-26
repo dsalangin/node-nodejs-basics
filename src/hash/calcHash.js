3c6fd8d018bef1951de0c21ff287a142eb1f0315
dsalangin <dsalangin74@gmail.com>
2025-04-26 20:35:50 +0300

    feat: implement calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const readStream = createReadStream(pathToFile);
+    const hash = createHash('SHA256');
+
+    readStream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();

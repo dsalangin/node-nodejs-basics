7ffa8f13c9fc1eb3f740a544cd1e58e5e65eeb55
dsalangin <dsalangin74@gmail.com>
2025-04-27 12:43:32 +0300

    feat: implement write file from console by stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { pipeline } from "stream/promises";
+import { createWriteStream } from "fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToWrite.txt');
+    await pipeline(
+        process.stdin,
+        createWriteStream(pathToFile)
+    )
 };
 
 await write();

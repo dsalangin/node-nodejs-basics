70690782d980e8256b1bf7575c11d6fd08ff303c
dsalangin <dsalangin74@gmail.com>
2025-04-27 13:06:40 +0300

    feat: implement reverse text

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    class ReverseStream extends Transform {
+        constructor(options) {
+            super(options);
+        }
+
+        _transform(chunk, _, cb) {
+            this.push(`${chunk.toString().split('').reverse().join('')}\n`);
+            cb();
+        }
+    }
+
+    await pipeline(
+        process.stdin,
+        new ReverseStream(),
+        process.stdout
+    );
 };
 
 await transform();

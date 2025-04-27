927ed21b0688a471c31a3f60f1ffcd03c0bd82f2
dsalangin <dsalangin74@gmail.com>
2025-04-27 12:37:52 +0300

    feat: implement read file by stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import {dirname, join} from 'path';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+
+    await pipeline(
+        createReadStream(pathToFile),
+        process.stdout,
+    )
 };
 
 await read();

7874105a715da29da5d1c5464c5e5002a1f51b39
dsalangin <dsalangin74@gmail.com>
2025-04-27 18:03:07 +0300

    feat: implement create child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,25 @@
+import { spawn } from 'child_process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToFile = join(__dirname, 'files', 'script.js');
+    const childProcess = spawn('node', [pathToFile, ...args], {
+        stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+    });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+
+    childProcess.on('error', (err) => {
+        throw err;
+    });
+
+    childProcess.stderr.on('data', (errData) => {
+        throw errData.toString();
+    });
 };
 
 // Put your arguments in function call to test this functionality
